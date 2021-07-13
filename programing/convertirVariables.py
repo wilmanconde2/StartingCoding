57838abf297a85a3210c39105952cd813e8a0041
WilmanConde <68202584+wilmanconde2@users.noreply.github.com>
2021-07-13 09:01:53 -0500

    actualizacion

--- programing/convertirVariables.py
+++
@@ -1,8 +0,0 @@
-# convirtiendo variables
-inicial = input('un numero...')
-x = str(inicial)    # x will be '#'
-y = int(inicial)    # y will be #
-z = float(inicial)  # z will be #.0
-print('como string',x)
-print('como entero',y)
-print('como flotante',z)
--- programing/promedio.py
+++
@@ -1,8 +0,0 @@
-print('Promedio de 5 números')      # muestra en consola para que sirve el programa
-n1 = float(input('1er número '))      # pide los 5 numeros al usuario
-n2 = float(input('2do número '))
-n3 = float(input('3er número '))
-n4 = float(input('4to número '))
-n5 = float(input('5to número '))
-promedio = float((n1+n2+n3+n4+n5)/5)      # variable que calcula el promedio
-print(f'El promedio es= {promedio:.4f}')   # muestra el resultado
